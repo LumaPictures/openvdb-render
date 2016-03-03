ba82e88a9c8b34e436ab8a8eece9c06808bda219
Pal Mezei <palm@luma-pictures.com>
2016-03-03 11:10:25 +1100

    Doing a test commit to see if it triggers an audit. Ref T10

--- plugin/main.cpp
+++ plugin/main.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
+
 #include <maya/MFnPlugin.h>
+#include <maya/MGlobal.h>
 
 #include <iostream>
 
@@ -9,12 +12,43 @@ MStatus initializePlugin(MObject obj)
 {
     MStatus status = MS::kSuccess;
 
+    if (MGlobal::mayaState() == MGlobal::kInteractive)
+    {
+        if (glewInit() != GLEW_OK)
+        {
+            status.perror("Error initializing glew.");
+            return status;
+        }
+    }
+
+    MFnPlugin plugin(obj, "Luma Pictures", "0.0.1", "Any");
+
+    status = plugin.registerShape(VDBVisualizerShape::typeName, VDBVisualizerShape::typeId,
+                                  VDBVisualizerShape::creator, VDBVisualizerShape::initialize,
+                                  VDBVisualizerShapeUI::creator, &VDBVisualizerShape::drawDbClassification);
+
+    if (!status)
+    {
+        status.perror("Error registering the VDBVisualizer node.");
+        return status;
+    }
+
     return status;
 }
 
 MStatus uninitializePlugin(MObject obj)
 {
     MStatus status = MS::kSuccess;
+
+    MFnPlugin plugin(obj);
+
+    status = plugin.deregisterNode(VDBVisualizerShape::typeId);
+
+    if (!status)
+    {
+        status.perror("Error deregistering the VDBVisualizer node.");
+        return status;
+    }
 
     return status;
 }
