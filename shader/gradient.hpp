6c0f671f67257d7a071e081e3ffb0f9832fda8ef
Pal Mezei <palm@luma-pictures.com>
2016-03-24 17:04:34 +1100

    Writing gradient query functions.

--- shader/gradient.hpp
+++ shader/gradient.hpp
@@ -3,6 +3,7 @@
 #include <ai.h>
 
 #include <string>
+#include <vector>
 
 class Gradient {
 private:
@@ -12,10 +13,31 @@ private:
         GRADIENT_RGB
     };
 
+    std::vector<float> m_float_ramp;
+    std::vector<AtRGB> m_rgb_ramp;
 
+    float m_contrast;
+    float m_input_min;
+    float m_input_max;
+    float m_bias;
+    float m_gain;
+    float m_output_min;
+    float m_output_max;
+    float m_gamma;
+    float m_hue_shift;
+    float m_saturation;
+    float m_exposure;
+    float m_multiply;
+    float m_add;
+    int m_gradient_type;
+    bool m_clamp_min;
+    bool m_clamp_max;
 
 public:
-    Gradient()
+    Gradient() : m_contrast(1.0f), m_input_min(0.0f), m_input_max(1.0f),
+                 m_bias(0.5f), m_gain(0.5f), m_output_min(0.0f), m_output_max(1.0f),
+                 m_gamma(1.0f), m_hue_shift(0.0f), m_saturation(1.0f), m_exposure(0.0f),
+                 m_multiply(1.0f), m_add(0.0f), m_gradient_type(GRADIENT_NONE), m_clamp_min(false), m_clamp_max(false)
     {
 
     }
@@ -49,9 +71,61 @@ public:
         AiParameterArray((base + "_rgb_ramp").c_str(), AiArray(1, 1, AI_TYPE_RGB, AI_RGB_BLACK));
     }
 
-    void update(const std::string&, AtNode*, AtParamValue*)
+    void update(const std::string& base, AtNode* node, AtParamValue*)
     {
 
+        m_contrast = AiNodeGetFlt(node, (base + "_contrast").c_str());
+        m_input_min = AiNodeGetFlt(node, (base + "_input_min").c_str());
+        m_input_max = AiNodeGetFlt(node, (base + "_input_max").c_str());
+        m_bias = AiNodeGetFlt(node, (base + "_bias").c_str());
+        m_gain = AiNodeGetFlt(node, (base + "_gain").c_str());
+        m_output_min = AiNodeGetFlt(node, (base + "_output_min").c_str());
+        m_output_max = AiNodeGetFlt(node, (base + "_output_max").c_str());
+        m_gamma = AiNodeGetFlt(node, (base + "_gamma").c_str());
+        m_hue_shift = AiNodeGetFlt(node, (base + "_hue_shift").c_str());
+        m_saturation = AiNodeGetFlt(node, (base + "_saturation").c_str());
+        m_exposure = AiNodeGetFlt(node, (base + "_exposure").c_str());
+        m_multiply = AiNodeGetFlt(node, (base + "_multiply").c_str());
+        m_add = AiNodeGetFlt(node, (base + "_add").c_str());
+        m_gradient_type = AiNodeGetInt(node, (base + "_gradient_type").c_str());
+        m_clamp_min = AiNodeGetBool(node, (base + "_clamp_min").c_str());
+        m_clamp_max = AiNodeGetBool(node, (base + "_clamp_max").c_str());
+
+        AtArray* arr = AiNodeGetArray(node, (base + "_float_ramp").c_str());
+        if (arr != nullptr)
+        {
+            const unsigned int nelements = arr->nelements;
+            if (m_float_ramp.size() != nelements)
+                std::vector<float>().swap(m_float_ramp);
+            else
+                m_float_ramp.clear();
+            if (nelements > 0)
+            {
+                m_float_ramp.reserve(nelements);
+                for (unsigned int i = 0; i < nelements; ++i)
+                    m_float_ramp.push_back(AiArrayGetFlt(arr, i));
+            }
+        }
+        else
+            std::vector<float>().swap(m_float_ramp);
+
+        arr = AiNodeGetArray(node, (base + "_float_ramp").c_str());
+        if (arr != nullptr)
+        {
+            const unsigned nelements = arr->nelements;
+            if (m_rgb_ramp.size() != nelements)
+                std::vector<AtRGB>().swap(m_rgb_ramp);
+            else
+                m_rgb_ramp.clear();
+            if (nelements > 0)
+            {
+                m_rgb_ramp.reserve(nelements);
+                for (unsigned int i = 0; i < nelements; ++i)
+                    m_rgb_ramp.push_back(AiArrayGetRGB(arr, i));
+            }
+        }
+        else
+            std::vector<AtRGB>().swap(m_rgb_ramp);
     }
 
     AtRGB evaluate(const AtRGB& input) const
@@ -63,7 +137,7 @@ public:
     {
         AtRGB input = AI_RGB_BLACK;
 
-        if (true) // check if float gradient
+        if (m_gradient_type == GRADIENT_FLOAT) // check if float gradient
         {
             float v = 0.0f;
             if (!AiVolumeSampleFlt(channel, interpolation, &v))
