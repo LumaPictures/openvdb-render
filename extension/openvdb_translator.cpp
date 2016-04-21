0990a24478aea0e640cc49cef2a94afe86129e53
Pal Mezei <palm@luma-pictures.com>
2016-04-21 16:24:02 +1000

    Fixing automated channel export. Ref T57

--- extension/openvdb_translator.cpp
+++ extension/openvdb_translator.cpp
@@ -17,40 +17,43 @@ AtNode* OpenvdbTranslator::CreateArnoldNodes()
     return volume;
 }
 
-void check_maya_node(MObject node, std::set<void*>& checked_maya_nodes, std::set<std::string>& out_grids);
+void check_arnold_nodes(AtNode* node, std::set<AtNode*>& checked_arnold_nodes, std::set<std::string>& out_grids)
+{
+    if (node == nullptr)
+        return;
 
-void check_maya_plug(MPlug plug, std::set<void*>& checked_maya_nodes, std::set<std::string>& out_grids)
-{
-    MPlugArray conns;
-    plug.connectedTo(conns, true, false);
-    const unsigned int conns_length = conns.length();
-    for (unsigned int i = 0; i < conns_length; ++i)
-        check_maya_node(conns[i].node(), checked_maya_nodes, out_grids);
-}
-// todo check the export arnold nodes, that could be faster!
-void check_maya_node(MObject node, std::set<void*>& checked_maya_nodes, std::set<std::string>& out_grids)
-{
-    MStatus status;
-    MFnDependencyNode dnode(node, &status);
-    if (!status)
+    if (checked_arnold_nodes.find(node) != checked_arnold_nodes.end())
         return;
-    void* user_node = dnode.userNode();
-    if (checked_maya_nodes.find(user_node) != checked_maya_nodes.end())
-        return;
-    checked_maya_nodes.insert(dnode.userNode());
-    // check if the MFnDependencyNode is something we want
-    MString type_name = dnode.typeName();
-    if (type_name == "aiVolumeSampleFloat" || type_name == "aiVolumeSampleRgb" || type_name == "vdb_sampler")
+
+    checked_arnold_nodes.insert(node);
+
+    auto check_channel = [&](const char* channel) {
+        const char* ch = AiNodeGetStr(node, channel);
+        if (ch != 0 && strlen(ch) > 0)
+            out_grids.insert(ch);
+    };
+
+    if (AiNodeIs(node, "volume_sample_float") || AiNodeIs(node, "volume_sample_rgb") || AiNodeIs(node, "openvdb_sampler"))
     {
-        MString channel = dnode.findPlug("channel").asString();
-        if (channel.length())
-            out_grids.insert(channel.asChar());
+        check_channel("channel");
+    }
+    else if (AiNodeIs(node, "volume_collector") || AiNodeIs(node, "openvdb_shader"))
+    {
+        check_channel("scattering_channel");
+        check_channel("attenuation_channel");
+        check_channel("emission_channel");
     }
 
-    // traverse other connections recursively
-    const unsigned int attribute_count = dnode.attributeCount();
-    for (unsigned int i = 0; i < attribute_count; ++i)
-        check_maya_plug(MPlug(node, dnode.attribute(i)), checked_maya_nodes, out_grids);
+    AtParamIterator* param_iter = AiNodeEntryGetParamIterator(AiNodeGetNodeEntry(node));
+    while (!AiParamIteratorFinished(param_iter))
+    {
+        const AtParamEntry* param_entry = AiParamIteratorGetNext(param_iter);
+        if (AiParamGetType(param_entry) == AI_TYPE_NODE)
+            check_arnold_nodes(reinterpret_cast<AtNode*>(AiNodeGetPtr(node, AiParamGetName(param_entry))), checked_arnold_nodes, out_grids);
+        else
+            check_arnold_nodes(AiNodeGetLink(node, AiParamGetName(param_entry)), checked_arnold_nodes, out_grids);
+    }
+    AiParamIteratorDestroy(param_iter);
 }
 
 void OpenvdbTranslator::Export(AtNode* volume)
@@ -62,32 +65,34 @@ void OpenvdbTranslator::Export(AtNode* volume)
     AiNodeDeclare(volume, "filename", "constant STRING");
     AiNodeSetStr(volume, "filename", FindMayaPlug("outVdbPath").asString().asChar());
 
+    ProcessParameter(volume, "min", AI_TYPE_POINT, "bboxMin");
+    ProcessParameter(volume, "max", AI_TYPE_POINT, "bboxMax");
+
+    AtNode* shader = nullptr;
+
+    if (FindMayaPlug("overrideShader").asBool())
+    {
+        const int instanceNum = m_dagPath.isInstanced() ? m_dagPath.instanceNumber() : 0;
+
+        MPlug shadingGroupPlug = GetNodeShadingGroup(m_dagPath.node(), instanceNum);
+        if (!shadingGroupPlug.isNull())
+        {
+            shader = ExportNode(shadingGroupPlug);
+            if (shader != 0)
+                AiNodeSetPtr(volume, "shader", shader);
+        }
+    }
+    else
+    {
+        shader = GetArnoldNode("shader");
+        AiNodeSetPtr(volume, "shader", shader);
+        ExportParams(shader);
+    }
+
     std::set<std::string> out_grids;
-    std::set<void*> checked_maya_nodes;
+    std::set<AtNode*> checked_arnold_nodes;
 
-    MFnDependencyNode dnode(GetMayaObject());
-    checked_maya_nodes.insert(dnode.userNode());
-    const char* channels_to_check[] = {"scattering_channel", "attenuation_channel", "emission_channel"};
-    for (auto channel : channels_to_check)
-    {
-        MString channel_value = FindMayaPlug(channel).asString();
-        if (channel_value.length())
-            out_grids.insert(channel_value.asChar());
-    }
-
-    const char* parameters_to_check[] = {
-        "scattering", "scattering_color", "scattering_intensity",
-        "attenuation", "attenuation_color", "attenuation_intensity",
-        "emission", "emission_color", "emission_intensity",
-        "anisotropy"
-    };
-
-    for (auto parameter : parameters_to_check)
-    {
-        MPlug plug = FindMayaPlug(parameter);
-        if (!plug.isNull())
-            check_maya_plug(plug, checked_maya_nodes, out_grids);
-    }
+    check_arnold_nodes(shader, checked_arnold_nodes, out_grids);
 
     MString additional_grids_string = FindMayaPlug("additional_channel_export").asString();
     MStringArray additional_grids;
@@ -111,34 +116,6 @@ void OpenvdbTranslator::Export(AtNode* volume)
 
     AiNodeDeclare(volume, "grids", "constant ARRAY STRING");
     AiNodeSetArray(volume, "grids", grid_names);
-
-    ProcessParameter(volume, "min", AI_TYPE_POINT, "bboxMin");
-    ProcessParameter(volume, "max", AI_TYPE_POINT, "bboxMax");
-
-    if (FindMayaPlug("overrideShader").asBool())
-    {
-        const int instanceNum = m_dagPath.isInstanced() ? m_dagPath.instanceNumber() : 0;
-
-        MPlug shadingGroupPlug = GetNodeShadingGroup(m_dagPath.node(), instanceNum);
-        if (!shadingGroupPlug.isNull())
-        {
-            AtNode* shader = ExportNode(shadingGroupPlug);
-            if (shader != 0)
-            {
-                AiNodeSetPtr(volume, "shader", shader);
-                AiNodeDeclare(volume, "mtoa_shading_groups", "constant ARRAY NODE");
-                AiNodeSetArray(volume, "mtoa_shading_groups", AiArray(1, 1, AI_TYPE_NODE, shader));
-            }
-        }
-    }
-    else
-    {
-        AtNode* shader = GetArnoldNode("shader");
-
-        AiNodeSetPtr(volume, "shader", shader);
-
-        ExportParams(shader);
-    }
 
     const float sampling_quality = FindMayaPlug("samplingQuality").asFloat();
     const float voxel_size = FindMayaPlug("voxelSize").asFloat();
