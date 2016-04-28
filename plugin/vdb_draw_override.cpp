1c20d6268a37e499fb33fe06d1a3ad5493394db4
Pal Mezei <palm@luma-pictures.com>
2016-04-28 14:25:05 +1000

    Fixing draw bug.

--- plugin/vdb_draw_override.cpp
+++ plugin/vdb_draw_override.cpp
@@ -612,6 +612,13 @@ namespace MHWRender {
 
                         glDrawArrays(GL_POINTS, 0, m_point_count);
 
+                        glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+                        glDepthMask(GL_TRUE);
+
+                        glDrawArrays(GL_POINTS, 0, m_point_count);
+
+                        glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+
                         glBindVertexArray(0);
                     }
 
